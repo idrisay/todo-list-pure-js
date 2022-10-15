81530fb326af77399dc4843c2850d2d9150fb27a
Idris AY <97283155+idrisay@users.noreply.github.com>
2022-10-15 18:02:30 +0200

    feat: add update todo

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const todoName = document.getElementById("todo-name");
 const addBtn = document.getElementById("add-new");
+const updateBtn = document.getElementById("update-todo");
 const todoList = document.getElementById("todo-list");
+let checkTodo = {};
 
-let todos = [{ id: 0, todo: "learn js", completed: false }];
+let todos = [
+  { id: 0, todo: "learn js", completed: false },
+  { id: 1, todo: "learn css", completed: false },
+  { id: 2, todo: "learn html", completed: false },
+];
 
 addBtn.onclick = () => {
   //   let newTodo = todoName.value;
@@ -14,7 +20,6 @@ addBtn.onclick = () => {
     todos = [...todos, newTodo];
     showTodos(todos);
     todoName.value = "";
-    console.log(todos);
     document.getElementById("error").classList.add("d-none");
   }
 };
@@ -29,9 +34,11 @@ const showTodos = (tds) => {
       todoList.innerHTML +=
         '<div class="list-group-item list-group-item-action mb-0 d-flex justify-content-between">' +
         element.todo +
-        "<div><i onclick='deleteTodo(" +
+        "<div><i onclick='editTodo(" +
         element.id +
-        ")' class='bi bi-trash p-4' style='cursor:pointer;'></i><i class='bi bi-pen'></i></div></div>";
+        ")' class='bi bi-pen px-2' style='cursor:pointer;'></i><i onclick='deleteTodo(" +
+        element.id +
+        ")' class='bi bi-trash px-2' style='cursor:pointer;'></i></div></div>";
     });
   }
 };
@@ -44,4 +51,24 @@ const deleteTodo = (id) => {
   const result = todos.filter((todo) => todo.id !== id);
   todos = result;
   showTodos(todos);
+};
+
+const editTodo = (id) => {
+  const result = todos.filter((todo) => todo.id == id);
+  checkTodo = result[0];
+  todoName.value = checkTodo.todo;
+  addBtn.classList.add("d-none");
+  updateBtn.classList.remove("d-none");
+};
+
+updateBtn.onclick = () => {
+  todos.forEach((element) => {
+    if (element.id == checkTodo.id) {
+      element.todo = todoName.value;
+    }
+  });
+  showTodos(todos);
+  todoName.value = "";
+  addBtn.classList.remove("d-none");
+  updateBtn.classList.add("d-none");
 };
